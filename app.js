9bbc614fa48f243331da755bb20fe6bfc98c448e
Prachodd <89415598+Prachodd@users.noreply.github.com>
2021-11-03 12:19:05 -0500

    updated

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var motorcycleRouter = require('./routes/motorcycle');
+var addmods = require('./routes/addmods');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/motorcycle', motorcycleRouter);
+app.use('/addmods', addmods);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
